52a3e51fd5dd475575aeb73ebb7b60fe7db5f6bc
Nicholas Dudfield <ndudfield@gmail.com>
2026-08-26 08:13:13 +0700

    test(provider): repin XFL activation fuel

--- python/jshookz/tests/test_runtime_types.py
+++ python/jshookz/tests/test_runtime_types.py
@@ -4,7 +4,7 @@ from jshookz.host import WasmHost
 from jshookz.paths import XAHAU_HOOK_PROVIDER_WASM
 from jshookz.runtime_types import SCHEMA, observe_runtime_types
 
-_NOMINAL_MATRIX_GAS = 11_163_604
+_NOMINAL_MATRIX_GAS = 11_158_530
 
 _XFL_VALUE_KERNEL = r"""
 JSON.stringify((() => {
